1dfdd844d9454e646a412bc9f691615e1f0e82ad
Andrew Ho <andrewyc.ho@gmail.com>
2015-04-07 20:43:00 +0100

    fixed some bugs
    the output looks good now

--- controller.erl
+++ controller.erl
@@ -6,11 +6,12 @@ main() ->
 	Pid_1 = spawn(fun temperature_converter:loop/0),
 	Pid_2 = spawn(fun temperature_converter:loop/0),
 	Pid_1 ! {"ConvertToCelsius",0.0},
-	Pid_1 ! {"ConvertToCelsius",25.0},
-	Pid_1 ! {"ConvertToCelsius",50.0},
-	Pid_1 ! {"ConvertToCelsius",100.0},
 	Pid_2 ! {"ConvertToFahrenheit",0.0},
-	Pid_2 ! {"ConvertToFahrenheit",25.0},
+	Pid_1 ! {"ConvertToFahrenheit",25.0},
+	Pid_2 ! {"ConvertToCelsius",25.0},
+	Pid_1 ! {"ConvertToCelsius",50.0},
 	Pid_2 ! {"ConvertToFahrenheit",50.0},
-	Pid_2 ! {"ConvertToFahrenheit",100.0}.
-
+	Pid_1 ! {"ConvertToFahrenheit",100.0},
+	Pid_2 ! {"ConvertToCelsius",100.0}.
+%	Pid_1 ! {"IncorrectFunctionName",100.0},
+%	Pid_2 ! {"ThisFunctionDoesNotExist",100.0}.
--- display.erl
+++ display.erl
@@ -6,10 +6,11 @@
 
 loop() -> receive
 	{Celsius,Fahrenheit} ->
-		C = erlang:float_to_list(Celsius),
-		F = erlang:float_to_list(Fahrenheit),
-		io:format("The temperature in Celsius is ~s!",[C]),
-		io:format("The temperature in Fahrenheit is ~s!",[F]),
+%		C = erlang:float_to_list(Celsius),
+%		F = erlang:float_to_list(Fahrenheit),
+		io:format("The temperature in Celsius is ~fC,~n and the equivalent temperature in Fahrenheit is ~fF!~n",[Celsius,Fahrenheit]),
+%		io:format("The temperature in Celsius is ~s!",[C]),
+%		io:format("The temperature in Fahrenheit is ~s!",[F]),
 		loop()
 	end.
 
